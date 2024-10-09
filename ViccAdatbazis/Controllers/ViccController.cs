3e0b56b1903935e384f1a9111161d9dac18c788c
Kristof <robertnagykristof@gmail.com>
2024-10-09 09:11:21 +0200

    semmi se mukodik nem értem

---
+++ ViccAdatbazis/Controllers/ViccController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViccAdatbazis.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViccAdatbazis.Controllers
+{
+    public class ViccController : Controller
+    {
+        private readonly ViccDbContext _context;
+
+        public ViccController(ViccDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vicc
+        public IActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var viccek = _context.ViccekTarolas
+                                  .Where(v => v.Aktiv)
+                                  .Skip((page - 1) * pageSize)
+                                  .Take(pageSize)
+                                  .ToList();
+            return View(viccek);
+        }
+
+    }
+}
--- ViccAdatbazis/Models/Vicc.cs
+++ ViccAdatbazis/Models/Vicc.cs
@@ -6,7 +6,7 @@
         public string Tartalom { get; set; }
         public string Feltolto { get; set; }
         public int Tetszik { get; set; }
-        public int NemTetszik { get; set; }
-        public bool Aktiv { get; set; }
+        public int NemTetszik { get; set; } 
+        public bool Aktiv { get; set; }  
     }
 }
